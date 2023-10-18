390d2c3315d425aaad7bdc8a00d0e5273b9498c0
Mike Panetta <panetta.mike@gmail.com>
2023-10-18 17:15:20 -0400

    More blinky, I2C scan.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,12 @@ use usb_pd::{
 #[rtic::app(device = rp2040_hal::pac, peripherals = true, dispatchers = [I2C0_IRQ])]
 mod app {
 
+    use defmt::info;
     use embedded_hal::digital::v2::OutputPin;
+    use embedded_hal::prelude::{
+        _embedded_hal_blocking_delay_DelayMs, _embedded_hal_blocking_i2c_Read,
+        _embedded_hal_blocking_i2c_Write, _embedded_hal_blocking_i2c_WriteRead,
+    };
     use fugit::{ExtU64, Instant};
     use rp_pico::hal::{
         self,
@@ -25,10 +30,21 @@ mod app {
         watchdog::Watchdog,
         Sio,
     };
-
     #[shared]
     struct Shared {
-        led: gpio::Pin<gpio::bank0::Gpio11, gpio::FunctionSio<gpio::SioOutput>, gpio::PullDown>,
+        leds: (
+            gpio::Pin<gpio::bank0::Gpio11, gpio::FunctionSio<gpio::SioOutput>, gpio::PullDown>,
+            gpio::Pin<gpio::bank0::Gpio12, gpio::FunctionSio<gpio::SioOutput>, gpio::PullDown>,
+            gpio::Pin<gpio::bank0::Gpio13, gpio::FunctionSio<gpio::SioOutput>, gpio::PullDown>,
+            gpio::Pin<gpio::bank0::Gpio14, gpio::FunctionSio<gpio::SioOutput>, gpio::PullDown>,
+        ),
+        i2c0: rp2040_hal::I2C<
+            I2C0,
+            (
+                rp2040_hal::gpio::Pin<gpio::bank0::Gpio0, gpio::FunctionI2c, gpio::PullDown>,
+                rp2040_hal::gpio::Pin<gpio::bank0::Gpio1, gpio::FunctionI2c, gpio::PullDown>,
+            ),
+        >,
         // pd: usb_pd::sink::Sink<
         //     fusb302b::Fusb302b<
         //         rp2040_hal::I2C<
@@ -105,51 +121,104 @@ mod app {
             &mut resets,
         );
 
-        // let i2c0_pins = (
-        //     // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
-        //     pins.gpio0.into_function::<gpio::FunctionI2C>(),
-        //     // UART RX (characters received by RP2040) on pin 2 (GPIO1)
-        //     pins.gpio1.into_function::<gpio::FunctionI2C>(),
-        // );
+        let i2c0_pins = (
+            // SDA0 (GPIO0)
+            pins.gpio0.into_function::<gpio::FunctionI2C>(),
+            // SCL0 (GPIO1)
+            pins.gpio1.into_function::<gpio::FunctionI2C>(),
+        );
 
-        // let i2c0 = i2c::I2C::i2c0(
-        //     c.device.I2C0,
-        //     i2c0_pins.0,
-        //     i2c0_pins.1,
-        //     fugit::RateExtU32::Hz(100000_u32),
-        //     &mut resets,
-        //     rp2040_hal::Clock::freq(&clocks.system_clock),
-        // );
+        let i2c0 = i2c::I2C::i2c0(
+            c.device.I2C0,
+            i2c0_pins.0,
+            i2c0_pins.1,
+            fugit::RateExtU32::Hz(100000_u32),
+            &mut resets,
+            rp2040_hal::Clock::freq(&clocks.system_clock),
+        );
 
         // let mut pd = usb_pd::sink::Sink::new(fusb302b::Fusb302b::new(i2c0), &crate::callback);
 
         // pd.init();
 
-        let mut led = pins.gpio11.into_push_pull_output();
-        led.set_high().unwrap();
+        let mut leds = (
+            pins.gpio11.into_push_pull_output(),
+            pins.gpio12.into_push_pull_output(),
+            pins.gpio13.into_push_pull_output(),
+            pins.gpio14.into_push_pull_output(),
+        );
+        leds.0.set_high().unwrap();
+        leds.1.set_high().unwrap();
+        leds.2.set_high().unwrap();
+        leds.3.set_low().unwrap();
 
         let mut timer = rp2040_hal::Timer::new(c.device.TIMER, &mut resets, &clocks);
         let alarm = timer.alarm_0().unwrap();
+        i2c0_task::spawn_after(1.millis()).unwrap();
         blink_led::spawn_after(500.millis()).unwrap();
 
         (
-            Shared { led },
+            Shared { leds, i2c0 },
             Local {},
             init::Monotonics(Monotonic::new(timer, alarm)),
         )
     }
 
+    #[task(shared = [i2c0])]
+    fn i2c0_task(mut c: i2c0_task::Context) {
+        info!("i2c0");
+
+        c.shared.i2c0.lock(|i2c0| {
+            for i in 0..=127 {
+                let mut readbuf: [u8; 1] = [0; 1];
+                let result = i2c0.read(i, &mut readbuf);
+                if let Ok(_d) = result {
+                    let mut readbuf: [u8; 0x43] = [0; 0x43];
+                    // Do whatever work you want to do with found devices
+                    info!("Device found at address {:?}", i);
+                    i2c0.write(i, &[0x0c, 0x01]).unwrap();
+                    i2c0.write(i, &[0x0b, 0x0f]).unwrap();
+                    i2c0.write(i, &[0x06, 0x00]).unwrap();
+                    i2c0.write(i, &[0x09, 0x07]).unwrap();
+
+                    i2c0.write_read(i, &[0x01], &mut readbuf).unwrap();
+                    info!("VER? {:x}", readbuf[0]);
+                    info!("SW0: {:x}", readbuf[1]);
+                    info!("SW1: {:x}", readbuf[2]);
+                    info!("MEA: {:x}", readbuf[3])
+                }
+            }
+            info!("Scan Done")
+        });
+    }
     #[task(
-        shared = [led],
-        local = [tog: bool = true],
+        shared = [leds],
+        local = [tog: bool = false, which: u8 = 0],
     )]
     fn blink_led(mut c: blink_led::Context) {
+        info!("BLINK {:?}", *c.local.which);
         if *c.local.tog {
-            c.shared.led.lock(|l| l.set_high().unwrap());
+            c.shared.leds.lock(|l| match *c.local.which {
+                0 => l.0.set_high().unwrap(),
+                1 => l.1.set_high().unwrap(),
+                2 => l.2.set_high().unwrap(),
+                3 => l.3.set_high().unwrap(),
+                4_u8..=u8::MAX => info!("WHAT?"),
+            });
         } else {
-            c.shared.led.lock(|l| l.set_low().unwrap());
+            c.shared.leds.lock(|l| match *c.local.which {
+                0 => l.0.set_low().unwrap(),
+                1 => l.1.set_low().unwrap(),
+                2 => l.2.set_low().unwrap(),
+                3 => l.3.set_low().unwrap(),
+                4_u8..=u8::MAX => info!("WHAT?"),
+            });
         }
-        *c.local.tog = !*c.local.tog;
+        *c.local.which = *c.local.which + 1;
+        if *c.local.which >= 4 {
+            *c.local.which = 0;
+            *c.local.tog = !*c.local.tog;
+        }
 
         //let now = monotonics::MyMono::now();
         //let foo: Instant<u64, 1, 1000> = Instant::<u64, 1, 1000>::from_ticks(now.ticks());
